68ba99f5af84cee2d3faa8d882edafee705402ca
Suman Choubey <86532093+suman2662021@users.noreply.github.com>
2022-08-30 15:29:54 +0530

    Update PrivateRoute.js

--- src/Routing/PrivateRoute.js
+++ src/Routing/PrivateRoute.js
@@ -5,5 +5,4 @@ const PrivateRoute = ({ child }) => {
     const { token } = useSelector(state => state.auth)
     return token === null ? child : <Navigate to='/' />
 }
-
 export default PrivateRoute;
